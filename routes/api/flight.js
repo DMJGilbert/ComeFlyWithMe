5e56b9fa51ea5a45f3992db93d4a5b4addf02096
Darren Gilbert <dmjgilbert@me.com>
2015-05-07 18:21:56 +0100

    Revert all the things

--- routes/api/flight.js
+++ routes/api/flight.js
@@ -2,17 +2,20 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-/* GET users listing. */
 router.get('/', function (req, res) {
-	if (req.query.id) {
-		request("http://bma.data.fr24.com/_external/planedata_json.1.4.php?f=" + req.query.id, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				res.send(body);
-			}
-		});
-	}else{
+	res.send({
+		error: 'Nope'
+	})
+});
+
+router.get('/search', function (req, res) {
+	if (req.query.query) {
 		res.send({
-			error:'Please provide id'
+			error: 'Nope'
+		})
+	} else {
+		res.send({
+			error: 'Please provide flight number'
 		})
 	}
 });
